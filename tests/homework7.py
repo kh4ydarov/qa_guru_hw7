b12d333442c937ec95c3fb6f633c00d85a48774d
kh4ydarov <kh4ydarovs@yandex.ru>
2024-04-06 11:00:20 +0500

    homework

--- tests/homework7.py
+++ tests/homework7.py
@@ -5,6 +5,13 @@ from pypdf import PdfReader
 from os_put import RESOURCE
 from openpyxl import load_workbook
 
+def test_pdf():
+    with zipfile.ZipFile(os.path.join(RESOURCE, 'file.zip')) as zip_file:
+        with zip_file.open('Python Testing with Pytest (Brian Okken).pdf') as pdf_file:
+            reader = PdfReader(pdf_file)
+            page = reader.pages[256]
+            text = reader.pages[2].extract_text()
+            assert 'Python Testing with pytest' in text
 
 
 
